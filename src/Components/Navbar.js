8d83c892119fd23744a2435eb8d01d4cd7a5de2a
Domenica <mimmalacava88@gmail.com>
2025-02-27 11:49:19 +0100

    modifica della barra di ricerca e dell'icona

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react"; //lo importiamo per poter scrivere i vari componenti
 import "./Navbar.css";
-
+import { FaSearch } from "react-icons/fa";
 
 
 const Navbar = () => {
@@ -10,15 +10,28 @@ const Navbar = () => {
             <div className="logo">
                 <img src="/images/Amazon_logo.svg" alt="Amazon Logo" />
             </div>
-            
+            <div className="search-container">
             <input type = "text" placeholder = "Cerca prodotti..." className="search-bar" /> {/* questa è la barra di ricerca*/}
+            <button className="search-button">
+                <FaSearch />
+            </button>
+            </div>
 
             <div className="account">
                 <span> Ciao, Accedi</span>
                 <div className="account icon">👤</div>
             </div>
+
+            <div className="orders">
+                <span>Resi</span>
+                <span>& Ordini</span>
+            </div>
+
             
-            <div className="cart">🛒 Carrello</div>
+            
+            <div className="cart">🛒 
+                <span className="cart-count">0</span>
+            </div>
         </nav>
     );
 };
